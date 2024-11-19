ae40dcf6e33eb5aaa879edb53c3f03c943c2c8ab
Rubina Karki <rubinakarki@Rubinas-MacBook-Pro.local>
2024-11-19 19:44:25 +0545

    [x] Updated logs for error handling

--- components/load_data.py
+++ components/load_data.py
@@ -11,4 +11,4 @@ def load_data_to_mongodb(df, db_name, collection_type):
         collection.insert_many(pandas_df.to_dict("records"))
         logging.info("Successfully loaded the processed data to database")
     except Exception as e:
-        logging.error("Cannot load the processed data to database")
+        logging.error(f"Error loading the processing data to database: {e}")
