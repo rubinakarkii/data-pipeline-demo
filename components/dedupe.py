4eff4d96708a3c0b795ce0864482e127f5fe0fce
Rubina Karki <rubinakarki@Rubinas-MacBook-Pro.local>
2024-11-20 11:04:08 +0545

    [x] Updated common.json schema

--- components/dedupe.py
+++ components/dedupe.py
@@ -2,7 +2,7 @@ import logging
 
 def merge_duplicates_from_transformed_data(transformed_data):
     logging.info("Finding and merging duplicates")
-    df_deduplicated = transformed_data.dropDuplicates(["_id", "hostname", "osVersion", "agentVersion", "platformName"])
+    df_deduplicated = transformed_data.dropDuplicates(["_id", "os"])
     logging.info("Merged duplicates")
     return df_deduplicated
 
